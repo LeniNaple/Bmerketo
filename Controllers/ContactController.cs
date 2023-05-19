3c47f2925935e4f27c4184f80082c07a4ccb95bc
SantaForse <santaforsell@gmail.com>
2023-05-19 21:13:10 +0200

    Started a bit on ContactViewModel and will continue wit db tomorrow.

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bmerketo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bmerketo.Controllers
 {
     public class ContactController : Controller
     {
+
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Index(ContactFormViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+
     }
 }
--- ViewModels/ContactFormViewModel.cs
+++ ViewModels/ContactFormViewModel.cs
@@ -4,10 +4,11 @@ namespace Bmerketo.ViewModels
 {
     public class ContactFormViewModel
     {
-
+        [Required(ErrorMessage = "An alias is required")]
         [DataType(DataType.Text)]
         public string Name { get; set; } = null!;
 
+        [Required(ErrorMessage = "An email is required")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; } = null!;
 
@@ -17,6 +18,7 @@ namespace Bmerketo.ViewModels
         [DataType(DataType.Text)]
         public string? Company { get; set; }
 
+        [Required(ErrorMessage = "You need to write a comment")]
         [DataType(DataType.Text)]
         public string Comment { get; set; } = null!;
 
